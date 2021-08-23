d10d25bedd176d97238152ddc1b27455a9b69b8b
Mason <87259832+datadawg-11@users.noreply.github.com>
2021-08-23 15:29:47 +0700

    Finished the updates to the HTML, js and css. Did not perform the optional task of gradient decrease

--- jsfile.js
+++ jsfile.js
@@ -1,64 +1,44 @@
-console.log('hello world')
 const container = document.querySelector('.container');
 const gridItems = document.querySelectorAll(".item");
 
 gridItems.forEach(gridItem => gridItem.addEventListener("mouseenter", event => {
     console.log(event);
     event.target.classList.add('itemHover')
+}));
 
-})
-);
+// const button = document.querySelector('.button');
+const button = document.querySelector('button')
 
-// mouseTarget.addEventListener("click", function(){ 
-//     console.log("Event triggered!"); });
+button.addEventListener('click', e => {
+    var x = prompt("Please enter a number between 1 and 100", "")
 
+    while(isNaN(x) || x < 1 || x >100) {
+        x = prompt("You did not enter a valid number. Please try again", "")
+    }
 
-// mouseTarget.forEach(mouseTargetItem => {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for (let i =0 ; i < (x*x); i++) {
+        const gridItems = document.createElement('div');
+        gridItems.textContent = i+1; 
+        gridItems.classList.add('item'); //add an item class to the div
+        container.appendChild(gridItems); //append it to the container div
+    }
+    const gridItems = document.querySelectorAll(".item");
+    gridItems.forEach(gridItem => gridItem.addEventListener("mouseenter", event => {
+        event.target.classList.add('itemHover')
+        })  
+    )
+    numColumns = `repeat(${x}, 1fr)`; //create a repeat string to factor in number of columns
+    container.style.gridTemplateColumns = numColumns
+    })
+    
+// // need a way to check if input is a valid number
+// var isNumber = Number.isInteger || function(number){ return !isNaN(parseFloat(n)) && isFinite(n) }
 
-//     mouseTargetItem.addEventListener('click', event => {
-//             console.log( event.target.id );
-//     });
-// });
-
-
-// const container = document.querySelector(".container");
-// makeGrid(16, 16);
-// const gridItem = document.querySelectorAll(".grid-item").forEach(gridItem => gridItem.addEventListener("click", myFunc));
-
-
-
-
-
-// <!-- <script>
-
-// var enterEventCount = 0;
-// var leaveEventCount = 0;
-// const mouseTarget = document.getElementById('mouseTarget');
-// const unorderedList = document.getElementById('unorderedList');
-
-// mouseTarget.addEventListener('mouseenter', e => {
-//   mouseTarget.style.border = '5px dotted orange';
-//   enterEventCount++;
-//   addListItem('This is mouseenter event ' + enterEventCount + '.');
-// });
-
-// mouseTarget.addEventListener('mouseleave', e => {
-//   mouseTarget.style.border = '1px solid #333';
-//   leaveEventCount++;
-//   addListItem('This is mouseleave event ' + leaveEventCount + '.');
-// });
-
-// function addListItem(text) {
-//   // Create a new text node using the supplied text
-//   var newTextNode = document.createTextNode(text);
-
-//   // Create a new li element
-//   var newListItem = document.createElement("li");
-
-//   // Add the text node to the li element
-//   newListItem.appendChild(newTextNode);
-
-//   // Add the newly created list item to list
-//   unorderedList.appendChild(newListItem);
+// var result = prompt("Enter your number");
+// if (!isNumber(result)){
+//    prompt("Please try again and enter your number");
 // }
-// </script> -->
+
